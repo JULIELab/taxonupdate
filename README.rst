a93dd09561d0e75d3bd0d1a6782d3212427f0ad5
pikatech <pikatech@mail.de>
2019-05-15 13:10:06 +0200

    Initial files

---
+++ README.rst
@@ -0,0 +1,24 @@
+Taxonomy Update for LINNAEUS
+============================
+
+This project contains the functions necessary to update the list of species used by LINNAEUS.
+
+  Gerner, M.; Nenadic, G. & Bergman, C. M.
+  LINNAEUS: A species name identification system for biomedical literature
+  BMC Bioinformatics, 2010, 11, 85
+
+---------------
+
+The species dictionaries available for LINNAEUS via https://sourceforge.net/projects/linnaeus/files/Entity_packs/ haven't been updated since 2011. To be able to tag the most recently discovered species, build a new one using the following lines of Python:::
+
+  from taxonomy_update import make_variants, taxonomy2dict
+
+  PREFIX = 'species:ncbi:'
+  tax = 'taxonomy.dat'
+  with open('dict-species.tsv', 'wt') as species:
+      for entry in taxonomy2dict(tax):
+          if entry['RANK'] != 'species':
+              continue
+          variants = sorted(make_variants(entry))
+          species.write(PREFIX + entry['ID'] + '\t' + '|'.join(variants) + '\n')
+
---
+++ taxonomy_update.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Functions to process the NCBI Taxonomy.
+"""
+
+from typing import Dict, List
+import logging
+import re
+
+FIELDS = ['ID', 'PARENT ID', 'RANK', 'GC ID', 'MGC ID', 'SCIENTIFIC NAME',
+          'ANAMORPH', 'BLAST NAME', 'COMMON NAME', 'EQUIVALENT NAME',
+          'GENBANK ACRONYM', 'GENBANK ANAMORPH', 'GENBANK COMMON NAME',
+          'GENBANK SYNONYM', 'IN-PART', 'MISNOMER', 'MISSPELLING', 'SYNONYM',
+          'TELEOMORPH', 'INCLUDES', 'ACRONYM']
+UNIQUE = frozenset(['ID', 'GC ID', 'MGC ID', 'PARENT ID', 'RANK'])
+
+TAXONOMIC = ['SCIENTIFIC NAME', 'SYNONYM', 'GENBANK SYNONYM',
+             'EQUIVALENT NAME', 'MISSPELLING', 'TELEOMORPH', 'ANAMORPH',
+             'GENBANK ANAMORPH', 'MISNOMER', 'IN-PART']
+COMMON_NAMES = ['GENBANK COMMON NAME', 'COMMON NAME', 'BLAST NAME']
+
+FIELD_REGEX = re.compile('(' + '|'.join(FIELDS) + r')\s+:\s')
+DELIMITER = re.compile('//')
+
+
+def taxonomy2dict(taxonomy: str) -> Dict:
+    """
+    Reads in the NCBI Taxonomy as processed by the European Bioinformatics
+    Institute.
+
+    taxonomy: path to the taxonomy file
+
+    Yields entries as dictionaries.
+    """
+    with open(taxonomy, 'rt') as tax:
+        first = True
+        entry = dict()
+        error = False
+        for i, line in enumerate(tax):
+            if error:
+                if DELIMITER.match(line):
+                    entry = dict()
+                    first = True
+                    error = False
+                    if 'ID' in entry:
+                        logging.warning('Skipped entry %d', entry['ID'])
+            elif first:
+                tax_id = FIELD_REGEX.match(line)
+                if tax_id is None:
+                    logging.warning('Missing ID on line %d', i)
+                    error = True
+                else:
+                    entry['ID'] = line[tax_id.end():].rstrip()
+                    first = False
+            elif DELIMITER.match(line):
+                yield entry
+                entry = dict()
+                first = True
+                error = False
+            else:
+                field = FIELD_REGEX.match(line)
+                if field is None:
+                    logging.warning('Unknown format on line %d', i)
+                    logging.warning('line %d: %s', i, line)
+                    error = True
+                else:
+                    match = FIELD_REGEX.match(line)
+                    if match:
+                        if field.group(1) in UNIQUE:
+                            entry[field.group(1)] = line[field.end():].rstrip()
+                        else:
+                            values = entry.get(field.group(1), [])
+                            values.append(line[field.end():].rstrip())
+                            entry[field.group(1)] = values
+
+
+def make_variants(tax_entry: Dict) -> List[str]:
+    """
+    Generates spelling variants as describes in the LINNAEUS paper.
+    """
+    variants = set()
+    for name in TAXONOMIC:
+        values = tax_entry.get(name, [])
+        for value in values:
+            tokens = value.split()
+            if len(tokens) == 1:
+                variants.append(value)
+            elif tokens[0].isalpha() and tokens[1].isalpha():
+                # Original spelling
+                variants.add(value)
+                # Original spelling, all lowercase
+                variants.add(value.lower())
+                # Abbreviated genus
+                tokens[0] = tokens[0][0] + '.'
+                variants.add(" ".join(tokens))
+                # Abbreviated genus, all lowercase
+                variants.add(" ".join(tokens).lower())
+                # Abbreviated genus, no space after genus
+                variant = tokens[0] + " ".join(tokens[1:])
+                variants.add(variant)
+                # Abbreviated genus, no space after genus, all lowercase
+                variants.add(variant.lower())
+            else:
+                variants.add(value)
+    for name in COMMON_NAMES:
+        values = tax_entry.get(name, [])
+        for value in values:
+            variants.add(value)
+    return variants
