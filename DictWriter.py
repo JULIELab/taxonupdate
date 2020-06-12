914c2b73e396a15cdf569e5045539cce0db7ab09
Tech <pikatech@mail.de>
2020-06-12 13:44:35 +0200

    Add sanity checks on file arguments

--- DictWriter.py
+++ DictWriter.py
@@ -6,48 +6,53 @@ Created on Fri Jun 12 11:12:11 2020
 @author: Kampe
 """
 import argparse
+from typing import Union
 import logging
+from pathlib import Path
+import sys
 from taxonomy_update import make_variants, taxonomy2dict
+
 
 class DictWriter:
     PREFIX = "species:ncbi:"
-    
+
     RANKS = [
-    "class",
-    "cohort",
-    "family",
-    "forma",
-    "genus",
-    "infraclass",
-    "infraorder",
-    "kingdom",
-    "no rank",
-    "order",
-    "parvorder",
-    "phylum",
-    "species",
-    "species group",
-    "species subgroup",
-    "subclass",
-    "subcohort",
-    "subfamily",
-    "subgenus",
-    "subkingdom",
-    "suborder",
-    "subphylum",
-    "subspecies",
-    "subtribe",
-    "superclass",
-    "superfamily",
-    "superkingdom",
-    "superorder",
-    "superphylum",
-    "tribe",
-    "varietas",
+        "class",
+        "cohort",
+        "family",
+        "forma",
+        "genus",
+        "infraclass",
+        "infraorder",
+        "kingdom",
+        "no rank",
+        "order",
+        "parvorder",
+        "phylum",
+        "species",
+        "species group",
+        "species subgroup",
+        "subclass",
+        "subcohort",
+        "subfamily",
+        "subgenus",
+        "subkingdom",
+        "suborder",
+        "subphylum",
+        "subspecies",
+        "subtribe",
+        "superclass",
+        "superfamily",
+        "superkingdom",
+        "superorder",
+        "superphylum",
+        "tribe",
+        "varietas",
     ]
 
-
-    def write(self, input: str, output: str, rank: str) -> int:
+    def write(
+        self, input: Union[Path, str], output: Union[Path, str], rank: str
+    ) -> int:
         counter = 0
         with open(output, "wt") as out:
             for entry in taxonomy2dict(input):
@@ -60,7 +65,9 @@ class DictWriter:
                 counter += 1
         return counter
 
+
 if __name__ == "__main__":
+    logging.basicConfig(format="%(levelname)s: %(message)s")
     PARSER = argparse.ArgumentParser(
         description="Writes a dictionary containing all entries of a specific rank"
     )
@@ -73,12 +80,31 @@ if __name__ == "__main__":
         type=str,
     )
     PARSER.add_argument(
-        "-i,", "--input", help="Path to taxonomy.dat", default="./taxonomy.dat", type=str
+        "-i,",
+        "--input",
+        help="Path to taxonomy.dat",
+        default="./taxonomy.dat",
+        type=str,
     )
     PARSER.add_argument(
-        "-o", "--output", help="Write into this file", default="./taxonomy.tsv", type=str
+        "-o",
+        "--output",
+        help="Write into this file",
+        default="./taxonomy.tsv",
+        type=str,
     )
     ARGS = PARSER.parse_args()
+    input = Path(ARGS.input)
+    if not input.exists():
+        logging.error("Input file %s does not exists.", input)
+        sys.exit(1)
+    if not input.is_file():
+        logging.error("Input argument %s is not a file.", input)
+        sys.exit(1)
+    output = Path(ARGS.output)
+    if output.exists():
+        logging.error("Output file %s already exists.", output)
+        sys.exit(1)
     writer = DictWriter()
     lines = writer.write(ARGS.input, ARGS.output, ARGS.rank)
     logging.info("%d lines written", lines)
