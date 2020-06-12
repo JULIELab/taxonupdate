191e6bdb83ec42ba56be0cc14f6d9d5d80ab13eb
Tech <pikatech@mail.de>
2020-06-12 11:52:28 +0200

    Make the conversion configurable
    The location of the input file, output file and the taxonomic rank that
    should be filtered for can now be specified via command line arguments.

---
+++ DictWriter.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 11:12:11 2020
+
+@author: Kampe
+"""
+import argparse
+import logging
+from taxonomy_update import make_variants, taxonomy2dict
+
+ranks = [
+    "class",
+    "cohort",
+    "family",
+    "forma",
+    "genus",
+    "infraclass",
+    "infraorder",
+    "kingdom",
+    "no rank",
+    "order",
+    "parvorder",
+    "phylum",
+    "species",
+    "species group",
+    "species subgroup",
+    "subclass",
+    "subcohort",
+    "subfamily",
+    "subgenus",
+    "subkingdom",
+    "suborder",
+    "subphylum",
+    "subspecies",
+    "subtribe",
+    "superclass",
+    "superfamily",
+    "superkingdom",
+    "superorder",
+    "superphylum",
+    "tribe",
+    "varietas",
+]
+
+
+class DictWriter:
+    PREFIX = "species:ncbi:"
+
+    def write(self, input: str, output: str, rank: str) -> int:
+        counter = 0
+        with open(output, "wt") as out:
+            for entry in taxonomy2dict(input):
+                if entry["RANK"] != rank:
+                    continue
+                variants = sorted(make_variants(entry))
+                _ = out.write(
+                    DictWriter.PREFIX + entry["ID"] + "\t" + "|".join(variants) + "\n"
+                )
+                counter += 1
+        return counter
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(
+        description="Writes a dictionary containing all entries of a specific rank"
+    )
+    PARSER.add_argument(
+        "-r",
+        "--rank",
+        choices=ranks,
+        help="Rank of the entry",
+        default="species",
+        type=str,
+    )
+    PARSER.add_argument(
+        "-i,", "--input", help="Path to taxonomy.dat", default="./taxonomy.dat", type=str
+    )
+    PARSER.add_argument(
+        "-o", "--output", help="Write into this file", default="./taxonomy.tsv", type=str
+    )
+    ARGS = PARSER.parse_args()
+    print(repr(ARGS))
+    writer = DictWriter()
+    lines = writer.write(ARGS.input, ARGS.output, ARGS.rank)
+    logging.info("%d lines written", lines)
