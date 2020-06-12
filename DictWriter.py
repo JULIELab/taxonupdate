f600f79e9c0d2a47c954d0737a43e212df5f8e39
Tech <pikatech@mail.de>
2020-06-12 13:11:23 +0200

    Remove print call

--- DictWriter.py
+++ DictWriter.py
@@ -9,43 +9,43 @@ import argparse
 import logging
 from taxonomy_update import make_variants, taxonomy2dict
 
+
 class DictWriter:
     PREFIX = "species:ncbi:"
-    
-    RANKS = [
-    "class",
-    "cohort",
-    "family",
-    "forma",
-    "genus",
-    "infraclass",
-    "infraorder",
-    "kingdom",
-    "no rank",
-    "order",
-    "parvorder",
-    "phylum",
-    "species",
-    "species group",
-    "species subgroup",
-    "subclass",
-    "subcohort",
-    "subfamily",
-    "subgenus",
-    "subkingdom",
-    "suborder",
-    "subphylum",
-    "subspecies",
-    "subtribe",
-    "superclass",
-    "superfamily",
-    "superkingdom",
-    "superorder",
-    "superphylum",
-    "tribe",
-    "varietas",
-    ]
 
+    RANKS = [
+        "class",
+        "cohort",
+        "family",
+        "forma",
+        "genus",
+        "infraclass",
+        "infraorder",
+        "kingdom",
+        "no rank",
+        "order",
+        "parvorder",
+        "phylum",
+        "species",
+        "species group",
+        "species subgroup",
+        "subclass",
+        "subcohort",
+        "subfamily",
+        "subgenus",
+        "subkingdom",
+        "suborder",
+        "subphylum",
+        "subspecies",
+        "subtribe",
+        "superclass",
+        "superfamily",
+        "superkingdom",
+        "superorder",
+        "superphylum",
+        "tribe",
+        "varietas",
+    ]
 
     def write(self, input: str, output: str, rank: str) -> int:
         counter = 0
@@ -60,6 +60,7 @@ class DictWriter:
                 counter += 1
         return counter
 
+
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
         description="Writes a dictionary containing all entries of a specific rank"
@@ -73,10 +74,18 @@ if __name__ == "__main__":
         type=str,
     )
     PARSER.add_argument(
-        "-i,", "--input", help="Path to taxonomy.dat", default="./taxonomy.dat", type=str
+        "-i,",
+        "--input",
+        help="Path to taxonomy.dat",
+        default="./taxonomy.dat",
+        type=str,
     )
     PARSER.add_argument(
-        "-o", "--output", help="Write into this file", default="./taxonomy.tsv", type=str
+        "-o",
+        "--output",
+        help="Write into this file",
+        default="./taxonomy.tsv",
+        type=str,
     )
     ARGS = PARSER.parse_args()
     print(repr(ARGS))
